c6444f5f77ce62a1209fe96774b75d9dd0afdbec
mrivera42 <1maxrivera@gmail.com>
2026-01-11 15:10:05 -0500

    Initial Commit

---
+++ Nunchuck Routine Tracker/Nunchuck_Routine_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nunchuck_Routine_TrackerApp.swift
+//  Nunchuck Routine Tracker
+//
+//  Created by Max Rivera on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Nunchuck_Routine_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
